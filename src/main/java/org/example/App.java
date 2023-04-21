c640a56e4efd67c5aecf97dbf03bca9fbce97a47
teodoramustata <teodora.mustata@gmail.com>
2023-04-21 01:18:32 -0700

    classes variables

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,29 @@
 package org.example;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Car carReference = new Car();
+        carReference.name = "Ferrari";
+        carReference.maxSpeed = 300;
+        carReference.fuelLevel = 60;
+        carReference.mileage = 12.5;
+        carReference.totalTraveledDistance = 0;
+        carReference.damaged = true;
+        carReference.doorCount = 1;
+        carReference.color = "Red";
+
+        //concatenation
+        System.out.println("Properties of car" + carReference.name);
+        System.out.println("Max speed:" + carReference.maxSpeed);
+        System.out.print("Fuel level:" + carReference.fuelLevel);
+        System.out.print("Mileage:" + carReference.mileage);
+        System.out.println("Total Traveled Distance:" + carReference.totalTraveledDistance);
+        System.out.println("Damaged:" + carReference.damaged);
+        System.out.print("Door Count" + carReference.doorCount);
+        System.out.print("Color:" + carReference.color);
+        System.out.println(carReference);
     }
 }
+v
---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Car {
+    String name;
+    double fuelLevel;
+    double mileage;
+    double totalTraveledDistance;
+    double maxSpeed;
+
+    int doorCount;
+    boolean damaged;
+    String color;
+}
